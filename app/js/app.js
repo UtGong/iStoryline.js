5eca0f5fd89e1d132a4265d1d99371707a2b43b2
JoJoGong <ugong@uw.edu>
2023-04-01 01:01:15 +0800

    demo

--- app/js/app.js
+++ app/js/app.js
@@ -1,42 +1,91 @@
-import { drawSquares } from '../../src/js/utils/drawer'
+import {
+  drawSquares,
+  drawStoryline,
+  drawGraphs,
+} from '../../src/js/utils/drawer'
+import { partitionResult } from '../../scripts/hClusterMDL'
 
+// Define the DOM elements and file path variable
 const peopleVolume = document.getElementById('people-volume')
 const peopleVolumeValue = document.getElementById('people-volume-value')
 const timeframesVolume = document.getElementById('timeframes-volume')
 const timeframesVolumeValue = document.getElementById('timeframes-volume-value')
 const locationsVolume = document.getElementById('locations-volume')
 const locationsVolumeValue = document.getElementById('locations-volume-value')
+let filePath = 'data/case/case1.json'
 
-let filePath = 'sim-front/Simulation-20-20-20.json'
+// Helper function to update graph
+function updateGraph() {
+  filePath = `sim-front/Simulation-${peopleVolume.value}-${timeframesVolume.value}-${locationsVolume.value}.json`
+  console.log('fileName :>> ', filePath)
+  fetch(filePath)
+    .then(response => response.json())
+    .then(data => {
+      const svg = Snap('#svg-container')
+      svg.selectAll('*').remove()
 
+      var storyJson = data
+      var partition = partitionResult(storyJson)
+      console.log('partition :>> ', partition)
+      var graphs = drawGraphs(storyJson, partition)
+      drawSquares(graphs)
+      const storylines = graphs[0].storylines
+      const characters = graphs[0].characters
+      storylines.forEach((storyline, idx) =>
+        drawStoryline(characters[idx], storyline)
+      )
+    })
+    .catch(error => {
+      console.error('Error loading story data:', error)
+    })
+}
+
+// Set initial values of volume sliders and file path
 peopleVolumeValue.innerHTML = peopleVolume.value
+timeframesVolumeValue.innerHTML = timeframesVolume.value
+locationsVolumeValue.innerHTML = locationsVolume.value
+
+// Add event listeners to update file path and volume values
 peopleVolume.addEventListener('input', function() {
   peopleVolumeValue.innerHTML = this.value
-  filePath = `sim-front/Simulation-${peopleVolume.value}-${timeframesVolume.value}-${locationsVolume.value}.json`
-  console.log('fileName :>> ', filePath)
+  updateGraph()
 })
 
-timeframesVolumeValue.innerHTML = timeframesVolume.value
 timeframesVolume.addEventListener('input', function() {
   timeframesVolumeValue.innerHTML = this.value
-  filePath = `sim-front/Simulation-${peopleVolume.value}-${timeframesVolume.value}-${locationsVolume.value}.json`
-  console.log('fileName :>> ', filePath)
+  updateGraph()
 })
 
-locationsVolumeValue.innerHTML = locationsVolume.value
 locationsVolume.addEventListener('input', function() {
   locationsVolumeValue.innerHTML = this.value
-  filePath = `sim-front/Simulation-${peopleVolume.value}-${timeframesVolume.value}-${locationsVolume.value}.json`
-  console.log('fileName :>> ', filePath)
+  updateGraph()
 })
 
-// svg
+function defaultGraph(filePath) {
+  console.log('fileName :>> ', filePath)
+  fetch(filePath)
+    .then(response => response.json())
+    .then(data => {
+      const svg = Snap('#svg-container')
+      svg.selectAll('*').remove()
 
-const partition = [2, 3] // Sample data
+      var storyJson = data
+      var partition = [50, 680, 1330]
 
-const padding = 100
+      var graphs = drawGraphs(storyJson, partition)
+      drawSquares(graphs)
+      const storylines = graphs[0].storylines
+      const characters = graphs[0].characters
+      storylines.forEach((storyline, idx) =>
+        drawStoryline(characters[idx], storyline)
+      )
+    })
+    .catch(error => {
+      console.error('Error loading story data:', error)
+    })
+}
 
-drawSquares(partition, padding)
+defaultGraph(filePath)
 
 // import { logStoryInfo } from '../../src/js/utils/logger'
 // import { drawStoryline } from '../../src/js/utils/drawer'
--- src/js/utils/drawer.js
+++ src/js/utils/drawer.js
@@ -1,8 +1,17 @@
-import Snap from 'snapsvg'
+import Snap, { color } from 'snapsvg'
 import * as d3 from 'd3'
+import { drawJointSubGraphs } from '../../../scripts/utils.js'
 
 export function drawSegmentPath(pathStr, defaultWidth = 2, hoverWidth = 4) {
-  const svg = Snap('#mySvg')
+  const svg = Snap('#svg-container')
+  const graphicsContainerHeight = document.querySelector('.graphics-container')
+    .clientHeight
+  const graphicsContainerWidth = document.querySelector('.graphics-container')
+    .clientWidth
+  svg.attr(
+    'viewBox',
+    `0 0 ${graphicsContainerWidth} ${graphicsContainerHeight}`
+  )
   const pathSvg = svg.path(pathStr)
   pathSvg.hover(
     () => {
@@ -75,60 +84,98 @@ function generateBezierPath(points) {
   return pathStr
 }
 
-export function drawSquares(partition, padding) {
+function getXScope(graphs) {
+  for (let graph of graphs) {
+    let minX = 10000
+    let maxX = 0
+    for (let nodeSetList of graph._nodes) {
+      for (let nodeSet of nodeSetList) {
+        let firstNode = nodeSet[0]
+        let lastNode = nodeSet[nodeSet.length - 1]
+        if (firstNode[0] < minX) {
+          minX = firstNode[0]
+        }
+        if (lastNode[0] > maxX) {
+          maxX = lastNode[0]
+        }
+      }
+    }
+    graph.maxX = maxX
+    graph.minX = minX
+    console.log('[minX, maxX] :>> ', [minX, maxX])
+  }
+  return graphs
+}
+
+export function drawSquares(graphs) {
   const svg = d3.select('#svg-container')
 
+  var minY = 80
+  const graphicsContainerHeight = document.querySelector('.graphics-container')
+    .clientHeight
+  const graphicsContainerWidth = document.querySelector('.graphics-container')
+    .clientWidth
+  var maxY = graphicsContainerHeight - minY
+
+  var _graphs = getXScope(graphs)
+
+  console.log('_graphs.length :>> ', _graphs.length)
+  for (let graph of _graphs) {
+    var minX = graph.minX
+    let maxX = graph.maxX
+    if (_graphs.indexOf(graph) === 0 && _graphs.length > 1) {
+      console.log('_graphs[1].minX  :>> ', _graphs[1].minX)
+      maxX = _graphs[1].minX - 100
+      console.log('first graph :>> ')
+    }
+
+    svg.attr(
+      'viewBox',
+      `0 0 ${graphicsContainerWidth} ${graphicsContainerHeight}`
+    )
+
+    const squareWidth = maxX - minX
+    const squareHeight = maxY - minY
+    console.log('[squareWidth, squareHeight] :>> ', [squareWidth, squareHeight])
+
+    svg
+      .append('rect')
+      .attr('x', minX)
+      .attr('y', minY)
+      .attr('width', squareWidth)
+      .attr('height', squareHeight)
+      .attr('fill', 'white')
+      .attr('stroke', 'blue')
+      .attr('stroke-width', 2)
+      .attr('rx', 10)
+      .attr('ry', 10)
+  }
+  console.log('done drawing squares :>> ')
+}
+
+export function drawGraphs(storyJson, partition, minY = 100) {
   const graphicsContainerHeight = document.querySelector('.graphics-container')
     .clientHeight
   const graphicsContainerWidth = document.querySelector('.graphics-container')
     .clientWidth
 
-  const numSquares = partition.length + 1
-  const squareWidth =
-    (graphicsContainerWidth - 40 - padding * (numSquares - 1)) / numSquares
-  const squareHeight = squareWidth
+  var width = graphicsContainerWidth - 20
 
-  const yOffset = (graphicsContainerHeight - squareHeight) / 2
+  var maxY = graphicsContainerHeight - minY
 
-  svg.attr(
-    'viewBox',
-    `0 0 ${graphicsContainerWidth} ${graphicsContainerHeight}`
+  // console.log('size :>> ', [width, maxY, minY])
+
+  var graphs = drawJointSubGraphs(
+    storyJson,
+    partition,
+    false,
+    width,
+    100,
+    minY,
+    maxY
   )
 
-  svg
-    .selectAll('rect')
-    .data(partition)
-    .enter()
-    .append('rect')
-    .attr('x', (d, i) => {
-      const xOffset =
-        (graphicsContainerWidth -
-          squareWidth * numSquares -
-          padding * (numSquares - 1)) /
-        2
-      return xOffset + i * (squareWidth + padding)
-    })
-    .attr('y', yOffset)
-    .attr('width', squareWidth)
-    .attr('height', squareHeight)
-    .attr('fill', 'white')
-    .attr('rx', 30) // set the x-axis radius of the corners to 5
-    .attr('ry', 30)
+  // console.log('graphs[0] :>> ', graphs[0])
 
-  svg
-    .append('rect')
-    .attr(
-      'x',
-      (numSquares - 1) * (squareWidth + padding) +
-        (graphicsContainerWidth -
-          squareWidth * numSquares -
-          padding * (numSquares - 1)) /
-          2
-    )
-    .attr('y', yOffset)
-    .attr('width', squareWidth)
-    .attr('height', squareHeight)
-    .attr('fill', 'white')
-    .attr('rx', 30) // set the x-axis radius of the corners to 5
-    .attr('ry', 30)
+  return graphs
 }
